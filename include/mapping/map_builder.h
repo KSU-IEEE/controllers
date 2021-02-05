229ee028bc21a6b66f178179f8b85d8857024c39
George Slagley <georgeslagley@gmail.com>
2021-02-05 13:57:46 -0500

    changed class notes to reference, changed old logic to txt notes, adding map_builder.h to stack

---
+++ include/mapping/map_builder.h
@@ -0,0 +1,44 @@
+//using namespace std;
+
+#include "mapping/map.h"
+namsepace pac_map{
+    controllers::Map m;
+
+    int wallThickness = 3;
+    int mapHeight = 96;
+    int mapWidth = 192;
+    int foot = 24;
+    int inch = 2;
+
+    //4 Outer Walls
+    m.addWall(0,0, wallThickness, mapHeight);
+    m.addWall(0,0,mapWidth,  wallThickness);
+    m.addWall(mapWidth-wallThickness, 0, wallThickness, mapHeight);
+    m.addWall(0, mapHeight-wallThickness, mapWidth, wallThickness);
+
+    //Rect A
+    m.addWall(0,0, foot + 6*inch + wallThickness, 8*inch + wallThickness);
+    //Rect B
+    m.addWall(3*foot + 5*inch, 0, foot + 2*inch, 8*inch+wallThickness);
+    //Rect D
+    m.addWall(0, wallThickness + 8*inch + 10*inch, foot+10*inch+1 + 2*wallThickness, 5*inch+1);
+
+    //Awkward wall between A and B
+    m.addWall(foot + 6*inch + wallThickness + 10*inch, 0, wallThickness, 8*inch + wallThickness);
+    //Vertcal wall to the right of F
+    m.addWall(2 * foot, mapHeight - (11*inch + 1), wallThickness, 11*inch+1);
+    //Horizontal wall above that wall
+    m.addWall(2*foot - (foot - wallThickness), mapHeight-(11*inch + 1 + wallThickness), foot, wallThickness);
+    //Short wall up and left from start
+    m.addWall(2*foot + wallThickness + 10*inch, mapHeight-(11*inch + 1 + wallThickness), 7*inch + 1, wallThickness);
+    //Long horizontal wall of T above start
+    m.addWall(foot + 10*inch + 1 + wallThickness + 10*inch, wallThickness+8*inch + 10*inch, 2*foot + 1*inch, wallThickness);
+    //Short vertical wall of T above start
+    m.addWall(foot + 10*inch + 1 + wallThickness + 10*inch + foot + wallThickness, wallThickness+8*inch + 10*inch + wallThickness, wallThickness, 5*inch);
+
+    m.reflectOnYAxis();
+
+    m.print();
+
+    return 0;
+}
