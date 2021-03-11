98b877f26e431212f6b34e8425382744f6aaea78
Trevor Stanca <tstanca@students.kennesaw.edu>
2021-03-11 13:42:15 -0500

    had to macro guard map_builder

--- include/mapping/map_builder.h
+++ include/mapping/map_builder.h
@@ -1,4 +1,6 @@
 //using namespace std;
+#ifndef MAP_BUILDER_H
+#define MAP_BUILDER_H
 
 #include <iostream>
 #include "mapping/map.h"
@@ -132,3 +134,4 @@ controllers::map build_config_space_map(controllers::map realMap){
 
 }
 }
+#endif
