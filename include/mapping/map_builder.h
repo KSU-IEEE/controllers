54d53714a65ae6b411cd7cc2aafc35520573a5ec
George Slagley <georgeslagley@gmail.com>
2021-03-11 16:49:51 -0500

    map builderr is still broken, swapped hardcoded vars forr calls to class for more dynamic fluidity

--- include/mapping/map_builder.h
+++ include/mapping/map_builder.h
@@ -26,44 +26,45 @@ controllers::map build_pm_real() {
 //function that builds the map for A*
 //hardcoded rn are the values for Southeastcon 2021 pacman
 controllers::map build_real_pman_map(){
-     int wallThickness = 3;
-     int mapHeight = 96;
-     int mapWidth = 192;
+     //int wallThickness = 3;
+     //int mapHeight = 96;
+     //int mapWidth = 192;
      int foot = 24;
      int inch = 2;
 
-    controllers::map realMap(mapHeight, mapWidth);
+    controllers::map realMap(192, 96);
 
     //setters called
     realMap.setWallThickness(3);
     realMap.setRobotSize(14);
-
+    realMap.setVmapCols(192);
+    realMap.setVmapRow(96);
     // controllers::map meep(mapHeight,mapWidth);
     //4 Outer Walls
-    realMap.addWall(0,0, wallThickness, mapHeight);
-    realMap.addWall(0,0,mapWidth,  wallThickness);
-    realMap.addWall(mapWidth-wallThickness, 0, wallThickness, mapHeight);
-    realMap.addWall(0, mapHeight-wallThickness, mapWidth, wallThickness);
-
+    realMap.addWall(0,0, realMap.getWallThickness(), realMap.getVmapRow());
+    realMap.addWall(0,0,realMap.getVmapCols(),  realMap.getWallThickness());
+    realMap.addWall(realMap.getVmapCols()-realMap.getWallThickness(), 0, realMap.getWallThickness(), realMap.getVmapRow());
+    realMap.addWall(0, realMap.getVmapRow()-realMap.getWallThickness(), realMap.getVmapCols(), realMap.getWallThickness());
+    std::cout << "Adding outer walls" << std::endl;
     //Rect A
-    realMap.addWall(0,0, foot + 6*inch + wallThickness, 8*inch + wallThickness);
+    realMap.addWall(0,0, foot + 6*inch + realMap.getWallThickness(), 8*inch + realMap.getWallThickness());
     //Rect B
-    realMap.addWall(3*foot + 5*inch, 0, foot + 2*inch, 8*inch+wallThickness);
+    realMap.addWall(3*foot + 5*inch, 0, foot + 2*inch, 8*inch+realMap.getWallThickness());
     //Rect D
-    realMap.addWall(0, wallThickness + 8*inch + 10*inch, foot+10*inch+1 + 2*wallThickness, 5*inch+1);
-
+    realMap.addWall(0, realMap.getWallThickness() + 8*inch + 10*inch, foot+10*inch+1 + 2*realMap.getWallThickness(), 5*inch+1);
+    std::cout << "Added rectangles" << std::endl;
     //Awkward wall between A and B
-    realMap.addWall(foot + 6*inch + wallThickness + 10*inch, 0, wallThickness, 8*inch + wallThickness);
+    realMap.addWall(foot + 6*inch + realMap.getWallThickness() + 10*inch, 0, realMap.getWallThickness(), 8*inch + realMap.getWallThickness());
     //Vertcal wall to the right of F
-    realMap.addWall(2 * foot, mapHeight - (11*inch + 1), wallThickness, 11*inch+1);
+    realMap.addWall(2 * foot, realMap.getVmapRow() - (11*inch + 1), realMap.getWallThickness(), 11*inch+1);
     //Horizontal wall above that wall
-    realMap.addWall(2*foot - (foot - wallThickness), mapHeight-(11*inch + 1 + wallThickness), foot, wallThickness);
+    realMap.addWall(2*foot - (foot - realMap.getWallThickness()), realMap.getVmapRow()-(11*inch + 1 + realMap.getWallThickness()), foot, realMap.getWallThickness());
     //Short wall up and left from start
-    realMap.addWall(2*foot + wallThickness + 10*inch, mapHeight-(11*inch + 1 + wallThickness), 7*inch + 1, wallThickness);
+    realMap.addWall(2*foot + realMap.getWallThickness() + 10*inch, realMap.getVmapRow()-(11*inch + 1 + realMap.getWallThickness()), 7*inch + 1, realMap.getWallThickness());
     //Long horizontal wall of T above start
-    realMap.addWall(foot + 10*inch + 1 + wallThickness + 10*inch, wallThickness+8*inch + 10*inch, 2*foot + 1*inch, wallThickness);
+    realMap.addWall(foot + 10*inch + 1 + realMap.getWallThickness() + 10*inch, realMap.getWallThickness()+8*inch + 10*inch, 2*foot + 1*inch, realMap.getWallThickness());
     //Short vertical wall of T above start
-    realMap.addWall(foot + 10*inch + 1 + wallThickness + 10*inch + foot + wallThickness, wallThickness+8*inch + 10*inch + wallThickness, wallThickness, 5*inch);
+    realMap.addWall(foot + 10*inch + 1 + realMap.getWallThickness() + 10*inch + foot + realMap.getWallThickness(), realMap.getWallThickness()+8*inch + 10*inch + realMap.getWallThickness(), realMap.getWallThickness(), 5*inch);
 
     realMap.reflectOnYAxis();
 
